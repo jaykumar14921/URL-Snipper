43002c1ebe6c0d0d508547d0734cd085caa2f568
Your Namejaykumar14921@gmail.comjay <you@example.com>
2025-04-22 23:34:41 +0530

    update

--- config/db.js
+++ config/db.js
@@ -8,7 +8,6 @@ const connectDB = async () => {
     try {
       await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
       });
       console.log('Database Connected');
     } catch (err) {
